dc690099e5649ba3818f8aa20acbd454e938362b
saptarsigeotech <saptarsi.debnath@geotechinfo.net>
2025-01-15 19:26:31 +0530

    userdetails created

--- src/features/application/application.routes.tsx
+++ src/features/application/application.routes.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable boundaries/element-types */
 import { RouteObject } from 'react-router-dom';
 import Home from './pages/Home';
-import Layout from '@/common/layouts/BasicLayout';
+// import UserDetails from './pages/UserDetails';
+import Layout from '@/common/layouts/ApplicationLayout';
 import NotFoundPage from '@/router/components/NotFoundPage';
 
 export const applicationcationRoutes: RouteObject[] = [
@@ -20,6 +21,10 @@ export const applicationcationRoutes: RouteObject[] = [
       {
         path: 'edit/:id',
         lazy: () => import('./pages/Edit'), // Edit route
+      },
+      {
+        path: 'user-details/:id',
+        lazy: () => import('./pages/UserDetails'), // user details route
       },
     ],
   },
